082e71573a4c3d37e242d4b01158c28a869ce47a
avril-tihista <imaviiii22@gmail.com>
2024-08-12 22:46:08 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Card from './Components/Card'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </>
+  )
+}
+
+export default App
